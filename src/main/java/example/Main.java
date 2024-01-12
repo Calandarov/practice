d31d53d0f223fbb66dab6d8f330d06debf3bf951
Kalandarov D <calandarov@icloud.com>
2024-01-12 15:21:52 +0300

    sync commit

--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -1,9 +1,6 @@
 package example;
 
 
-import practice.task14.StringUtilsCamel;
-import practice.task19.OneDimensionalArrayIterator;
-
 public class Main {
     public static void main(String[] args) {}
 }
