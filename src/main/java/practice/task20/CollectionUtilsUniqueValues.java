098109b12c30c3aa29116d9088ba93fd8ac68028
Kalandarov D <calandarov@icloud.com>
2024-01-12 11:01:03 +0300

    add task20

---
+++ src/main/java/practice/task20/CollectionUtilsUniqueValues.java
@@ -0,0 +1,14 @@
+/*
+* Реализуйте метод, который получает коллекцию объектов, и на выходе возвращает коллекцию уникальных значений
+*/
+package practice.task20;
+
+import java.util.*;
+
+public class CollectionUtilsUniqueValues {
+    private CollectionUtilsUniqueValues() {}
+    public static <T> Collection<T> removeDuplicates(Collection<T> inputCollection) {
+
+        return List.of();
+    }
+}
---
+++ src/test/java/practice/task20/CollectionUtilsUniqueValuesTest.java
@@ -0,0 +1,10 @@
+package practice.task20;
+
+import org.junit.Test;
+
+public class CollectionUtilsUniqueValuesTest {
+    @Test
+    public void removeDuplicates() {
+//        CollectionUtilsUniqueValues.removeDuplicates();
+    }
+}
