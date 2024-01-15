55cb973c71e806637253275aeb05c3113679f8a9
Calandarov <calandarov@icloud.com>
2024-01-15 18:11:35 +0300

    task21 done

---
+++ src/main/java/practice/task21/CollectionUtilsCount.java
@@ -0,0 +1,24 @@
+/*
+* Реализуйте метод, который принимает массив элементов
+* и возвращает Map <K, Integer> где K - элемент массива, Integer — количество таких элементов в массиве
+*/
+package practice.task21;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CollectionUtilsCount {
+    private CollectionUtilsCount() {}
+
+    public static <K> Map<K, Integer> countElements(K[] inputArray) {
+        Map<K, Integer> map = new HashMap<>();
+
+        for(K item : inputArray) {
+            int value = map.getOrDefault(item, 0) + 1;
+            map.put(item, value);
+        }
+
+//        System.out.println(map);
+        return map;
+    }
+}
---
+++ src/test/java/practice/task21/CollectionUtilsCountTest.java
@@ -0,0 +1,23 @@
+package practice.task21;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CollectionUtilsCountTest {
+    @Test
+    public void countElements() {
+        String[] inputArr = {"1", "2", "4", "1", "1", "1", "2", "3", "2"};
+        Map<String, Integer> actualMap = new HashMap<>(CollectionUtilsCount.countElements(inputArr));
+        Map<String, Integer> expectedMap = new HashMap<>();
+        expectedMap.put("1", 4);
+        expectedMap.put("2", 3);
+        expectedMap.put("3", 1);
+        expectedMap.put("4", 1);
+
+        Assert.assertEquals(actualMap, expectedMap);
+    }
+}
