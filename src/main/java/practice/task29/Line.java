4592bd06e6d8674b05ea699c327666810eca3b2c
Calandarov <calandarov@icloud.com>
2024-01-25 19:24:13 +0300

    доделать 29 таску, класс Line

---
+++ src/main/java/practice/task29/Line.java
@@ -0,0 +1,47 @@
+package practice.task29;
+
+public class Line {
+    private int startPointX;
+    private int startPointY;
+    private int endPointX;
+    private int endPointY;
+
+    public Line(int startPointX, int startPointY, int endPointX, int endPointY) {
+        this.startPointX = startPointX;
+        this.startPointY = startPointY;
+        this.endPointX = endPointX;
+        this.endPointY = endPointY;
+    }
+
+    public int getStartPointX() {
+        return startPointX;
+    }
+
+    public void setStartPointX(int startPointX) {
+        this.startPointX = startPointX;
+    }
+
+    public int getStartPointY() {
+        return startPointY;
+    }
+
+    public void setStartPointY(int startPointY) {
+        this.startPointY = startPointY;
+    }
+
+    public int getEndPointX() {
+        return endPointX;
+    }
+
+    public void setEndPointX(int endPointX) {
+        this.endPointX = endPointX;
+    }
+
+    public int getEndPointY() {
+        return endPointY;
+    }
+
+    public void setEndPointY(int endPointY) {
+        this.endPointY = endPointY;
+    }
+}
